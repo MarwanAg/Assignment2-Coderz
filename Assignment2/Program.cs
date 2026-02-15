dbbf8f769d539b3e0209fe4e255b8b70adb66e41
Marwan AbuGhazalah <Marwanabughazalah@gmail.com>
2026-02-15 21:28:43 +0300

    Add project files.

---
+++ Assignment2/Program.cs
@@ -0,0 +1,39 @@
+﻿//decleration + initialization
+Dictionary<char, int> dictionary = new Dictionary<char, int>();
+Console.WriteLine("Enter the word: ");
+string text = Convert.ToString(Console.ReadLine());   //string text = "successfully";
+int counter = 0;
+bool exists = false;
+
+//processing
+foreach (char ch1 in text)
+{
+    counter = 0;
+    exists = false;
+    foreach (char ch2 in text)
+    {
+        if (ch1 == ch2)
+        {
+            counter++;
+        }
+    }
+    foreach (var item in dictionary)
+    {
+        if (item.Key == ch1)
+        {
+            exists = true;
+            break;
+        }
+
+    }
+    if (!exists)
+    {
+        dictionary.Add(ch1, counter);
+    }
+}
+
+//print
+foreach (var item in dictionary)
+{
+    Console.WriteLine($"{item.Value} : {item.Key}");
+}
